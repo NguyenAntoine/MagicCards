641e2ceb0da29d984e6f0dfd673a540341a851fe
Antoine <antoine.nguyen3@epsi.fr>
2018-10-03 17:29:51 +0200

    Update app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, LayoutAnimation, View, Image, Touchable} from 'react-native';
+import {StyleSheet, Animated, View, Image, PanResponder} from 'react-native';
 
 export default class App extends React.Component {
     imgDimensions = {
@@ -7,37 +7,92 @@ export default class App extends React.Component {
         height: 536
     };
 
-    _onPress = (x, y) => {
-        // Animate the update
-        LayoutAnimation.spring();
+    constructor(props){
+        super(props);
 
-        // this make the center move directly
-        this.x.setValue(x);
-        this.y.setValue(y);
-    };
+        this.state = {
+            showDraggable   : true,
+            dropZoneValues  : null,
+            pan             : new Animated.ValueXY()
+        };
 
-    render() {
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder : () => true,
+            onPanResponderMove           : Animated.event([null,{
+                dx : this.state.pan.x,
+                dy : this.state.pan.y
+            }]),
+            onPanResponderRelease        : (e, gesture) => {
+                if(this.isDropZone(gesture)) {
+                    this.setState({
+                        showDraggable : false
+                    });
+                } else {
+                    Animated.spring(
+                        this.state.pan,
+                        {toValue: {x: 0, y: 0}}
+                    ).start();
+                }
+            }
+        });
+    }
+
+    isDropZone(gesture){
+        let dz = this.state.dropZoneValues;
+        return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+    }
+
+    setDropZoneValues(event){
+        this.setState({
+            dropZoneValues : event.nativeEvent.layout
+        });
+    }
+
+    render(){
         return (
-            <View style={styles.container}>
-                <Touchable onPress={this._onPress(this.x, this.y)}>
-                    <Image
-                        source={require('./assets/cards-back-red.png')}
-                        style={{
-                            width: this.imgDimensions.width,
-                            height: this.imgDimensions.height
-                        }}
-                    />
-                </Touchable>
+            <View
+                onLayout={this.setDropZoneValues.bind(this)}
+                style={styles.mainContainer}>
+                <View style={styles.dropZone}>
+                </View>
+
+                {this.renderImage()}
             </View>
         );
+    }
+
+
+    renderImage() {
+        // if(this.state.showDraggable) {
+            return (
+                <View style={styles.container}>
+                    <Animated.View
+                        {...this.panResponder.panHandlers}
+                        style={[this.state.pan.getLayout()]}>
+                        <Image
+                            source={require('./assets/cards-back-red.png')}
+                            style={{
+                                width: this.imgDimensions.width,
+                                height: this.imgDimensions.height
+                            }}
+                        />
+                    </Animated.View>
+                </View>
+            );
+        // }
     }
 }
 
 const styles = StyleSheet.create({
+    mainContainer: {
+        flex    : 1
+    },
+    dropZone    : {
+        height         : 100
+    },
     container: {
-        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
 });
