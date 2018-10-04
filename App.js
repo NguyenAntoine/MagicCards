ce2355aceab0983c5c7a064af4e63f4bfa70a9a4
Antoine <antoine.nguyen3@epsi.fr>
2018-10-04 10:30:59 +0200

    Order change for import

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-    StyleSheet,
     Animated,
-    View,
+    Dimensions,
     Image,
     PanResponder,
-    Dimensions
+    StyleSheet,
+    View,
 } from 'react-native';
 
 export default class App extends React.Component {
